34b799aad5d56a9920acffd2eba9114ede799102
imbasicprogrammer <mhabibmhabib05@gmail.com>
2025-01-12 17:42:30 +0700

    belajar tipe data  NaN dan undefined

--- script.js
+++ script.js
@@ -1,28 +1,17 @@
-//Belajar tipe data Boolean
+//tipe data spesial
+// NaN dan Undefined
 
-let dataBool = true; //true = 1
-console.log(dataBool);
+//1 NaN (not a number)
+//operasi yang menghasilkan bukan angka
+console.log(`akar dari -1 ${Math.sqrt(-1)}`);
+console.log(`"otong" / "ucup" = ${"otong" / "ucup"}`);
+console.log(`"otong" * "ucup" = ${"otong" * "ucup"}`);
+console.log(`"otong" - "ucup" = ${"otong" - "ucup"}`);
+console.log(`"otong" + "ucup" = ${"otong" + "ucup"}`);
 
-dataBool = false; //false = 0
-console.log(dataBool);
-
-//boolean bisa juga di definisikan dengan ->  <,>,==
-let data_bool = 3 < 9;
-console.log(`3 < 9 = ${data_bool}`);
-
-data_bool = 3 > 9;
-console.log(`3 > 9 = ${data_bool}`);
-
-nama = "luffy" == "sabo"; //persamaan dg string
-console.log(`luffy == sabo -> ${nama}`);
-
-//nilai boolean khusus false bisa juga
-console.log("nilai false akan true atau sama dengan :");
-data_bool2 = false == 0;
-console.log(`false == 0 -> ${data_bool2}`);
-
-data_bool2 = false == 0n;
-console.log(`false == 0n -> ${data_bool2}`);
-
-data_bool2 = false == " ";
-console.log(`false == " " -> ${data_bool2}`);
+//2 Undefined
+//adalah variabel atau fungsi yang tak memiliki nilai
+//contoh:
+let a;
+console.log(`a belum memiliki nilai -> ${a}`);
+console.log(console.log("test")); //console.log tak memuat nilai jadi setelah tercetak maka console.log undefined
