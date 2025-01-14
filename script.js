6280049972c7ddd2ef78888fd3d8bd23400f5e6e
imbasicprogrammer <mhabibmhabib05@gmail.com>
2025-01-14 15:32:05 +0700

    belajar operator komparasi dan menambahkan codesnap

--- script.js
+++ script.js
@@ -1,22 +1,40 @@
-//belajar Operator logika
+//operasi Komparasi
+// 1. Equality (==)
+// 2. !=  -> inequality
+// 3. === -> stric Equality
+// 4. !== -> strin ineaquality
+// 5. >   -> lebih besar dari
+// 6. >=  -> lebih besar sama dengan
+// 7. <   -> lebih kecil dari
+// 8. <=  -> lebih kecil sama dengan
 
-//or, not, and
+//1. >  -> besar dari
+let dataBool = 10 > 7;
+console.log(`10 > 7 = ${dataBool}\n`);
 
-let dataBool = true;
-console.log(`dataBool = ${dataBool}`);
-//1. Not (!) -> membalikan nilai
+//2. >=  -> besar sama dari
+console.log(`10 >= 10 ${10 >= 10}`);
 
-console.log(`!dataBool = ${!dataBool}`);
-//selain true dan false, bisa juga dengan 0(false) dan 1 - bil bulat > 0(true)
+//3. < kecil dari
+console.log(`7 < 10 ${7 < 10}`);
 
-//2.Or (||)
-console.log(`True || True = ${true || true}`);
-console.log(`True || false = ${true || false}`);
-console.log(`false || True = ${false || true}`);
-console.log(`false || false = ${false || false}`);
+//4. <= kecil sama dari
+console.log(`7 <- 7 ${7 <= 7}`);
 
-//3 And (&&)
-console.log(`True && True = ${true && true}`);
-console.log(`True && false = ${true && false}`);
-console.log(`false && True = ${false && true}`);
-console.log(`false && false = ${false && false}`);
+//5 == -> equality
+console.log(`11 == 11 ${11 == 11}`);
+console.log(`7 == "7" ${7 == "7"}`); // dengan == maka string juga termasuk
+
+//6. != -> inequality
+console.log(`12 != 13 ${12 != 13}`);
+console.log(`3 != "3" ${3 != "3"}`); // sama dengan enquality
+
+//7. ===  -> strict equality (string tidak termasuk)
+console.log(`14 === 14 ${14 === 14}`);
+console.log(`14 === "14" ${14 === "14"}`);
+console.log(`"14" === "14" ${"14" === "14"}`);
+
+//8. !== -> strict inequality
+console.log(`15 !== 15 ${15 !== 15}`);
+console.log(`15 !== "15" ${15 !== "15"}`);
+console.log(`15 !== "15" ${"15" !== "15"}`);
