f4112b96fab6402175d673fecb8e224605fbb667
imbasicprogrammer <mhabibmhabib05@gmail.com>
2025-01-16 15:22:57 +0700

    latihan juragan angkot 2, dengan for

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 //1. juragan angkot memiliki sepuluh angkot dalam kondisi bagus, buatkan loop untuk menomori tiap angkot
 
-angkot = 1;
+/*
+let angkot = 1;
 
 while (angkot <= 10) {
   console.log(`angkot ${angkot} dalam kondisi baik`);
   angkot++;
 }
+*/
+
+//2. angkot 7 - 10 sedang tak beroperasi menggunakan for
+let jmlAngkot = 10;
+let angkotBeroperasi = 6;
+let noAngkot = 1;
+
+while (noAngkot <= angkotBeroperasi) {
+  console.log(`Angkot no ${noAngkot} bisa beroperasi`);
+  noAngkot++;
+}
+
+angkotBeroperasi++;
+
+for (angkotBeroperasi; angkotBeroperasi <= jmlAngkot; angkotBeroperasi++) {
+  console.log(`Angkot no ${angkotBeroperasi} tidak bisa beroperasi`);
+}
+
+console.log("program berakhir");
