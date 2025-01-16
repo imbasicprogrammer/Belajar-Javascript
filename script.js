409bdaa3553fad5537d3144fd8b8145083560469
imbasicprogrammer <mhabibmhabib05@gmail.com>
2025-01-16 17:31:01 +0700

    belajar switch

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
-//belajar perulangan while
+//belajar switch
 
-//1. while
-angka = 1;
-while (angka <= 10) {
-  console.log(`angka ${angka}`);
-  angka++;
+menu = prompt("pilih menu : \n cth: daging, ayam, ikan, burger, pizza, mie instan");
+
+switch (menu) {
+  case "daging":
+    console.log("daging sehat!!");
+    alert("daging sehat");
+    break;
+  case "ayam":
+    console.log("ayam sehat!!");
+    alert("ayam sehat");
+    break;
+  case "ikan":
+    console.log("ikan sehat!!");
+    alert("ayam sehat");
+    break;
+  case "burger":
+    console.log("burger tidak sehat");
+    alert("burger tidak sehat");
+    break;
+  case "pizza":
+    console.log("pizza tidak sehat!!");
+    alert("pizza tidak sehat");
+    break;
+  case "mie instan":
+    console.log("mie instan tidak sehat!!");
+    alert("mie instan tidak sehat");
+    break;
+  default:
+    alert("yang anda masukan salah!!");
 }
-
-console.log("program selesai");
