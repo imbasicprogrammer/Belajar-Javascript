485a6bb8ded9ea8b6c9ac8432cdad0054bc4e027
imbasicprogrammer <mhabibmhabib05@gmail.com>
2025-01-16 16:40:57 +0700

    latihan juragan angkot dengan if else if yang lebih kompleks

--- script.js
+++ script.js
@@ -5,8 +5,10 @@ let angkotBeroperasi = 6;
 let noAngkot = 1;
 
 for (noAngkot; noAngkot <= jmlAngkot; noAngkot++) {
-  if (noAngkot <= angkotBeroperasi) {
+  if (noAngkot <= angkotBeroperasi && noAngkot !== 5) {
     console.log(`angkot no ${noAngkot} bisa beropasi`);
+  } else if (noAngkot === 8 || noAngkot === 10 || noAngkot == 5) {
+    console.log(`angkot no ${noAngkot} sedang lembur`);
   } else {
     console.log(`angkot no ${noAngkot} tidak bisa beroperasi`);
   }
