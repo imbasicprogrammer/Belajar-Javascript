6693a5e54ccbd5827b40090f8223581ef3e95f42
imbasicprogrammer <mhabibmhabib05@gmail.com>
2025-01-20 18:55:12 +0700

    games suit sederhana

--- script.js
+++ script.js
@@ -1,66 +1,37 @@
-//Belajar perulangan dan pengkondisian bersarang
+//A. membuat game suit sumatera barat
+let main = true;
+while (main == true) {
+  //mengambil input user
+  let user = prompt("silahkan pilih : gajah, semut, orang");
 
-//1. perulangan menampilkan bintang 10 baris dengan masing masing 5 bintang
-/*
-let string = "";
-for (let i = 0; i < 10; i++) {
-  for (let j = 0; j < 5; j++) {
-    string += "*";
+  //mengambil input computer dengan fungsi random
+  let compt = Math.random();
+
+  if (compt < 0.34) {
+    compt = "gajah";
+  } else if (compt > 0.34 && compt < 0.78) {
+    compt = "orang";
+  } else {
+    compt = "semut";
   }
-  string += "\n";
+
+  hasil = "";
+  if (user == compt) {
+    hasil = "Seri";
+  } else if (user == "gajah") {
+    hasil = compt == "orang" ? "Anda menang" : "anda kalah"; //pakai ternary
+  } else if (user == "orang") {
+    hasil = compt == "semut" ? "anda menang" : "anda kalah";
+  } else if (user == "semut") {
+    hasil = compt == "gajah" ? "anda menang" : "anda kalah";
+  } else {
+    hasil = "pilihan anda salah";
+  }
+
+  alert(`kamu : ${user}\ncomputer : ${compt}\nhasil = ${hasil}`);
+  console.log(`compt = ${compt} vs user = ${user} --> ${hasil}`);
+  main = confirm("mau lanjut main ?");
 }
-console.log(string);
-*/
 
-//2. Perulangan menampilkan bintang segitiga
-/*
-let star = "";
-for (let i = 0; i < 10; i++) {
-  for (let j = 0; j <= i; j++) {
-    star += "*";
-  }
-  star += "\n";
-}
-console.log(star);
-*/
-
-//3. perulangan bintang segitiga terbalik
-/*
-let bintang = "";
-for (let i = 10; i > 0; i--) {
-  for (let j = 0; j < i; j++) {
-    bintang += "*";
-  }
-  bintang += "\n";
-}
-console.log(bintang); */
-
-//4. segitiga terbalik berlawanan
-/*
-let bintang = "";
-for (let i = 0; i < 10; i++) {
-  for (let j = 0; j < i; j++) {
-    bintang += " ";
-  }
-  for (let k = 10; k > i; k--) {
-    bintang += "*";
-  }
-  bintang += "\n";
-}
-console.log(bintang); */
-
-//5 Membuat segitiga sama kaki
-
-let bintang = "";
-for (let i = 1; i <= 10; i++) {
-  // Tambahkan spasi untuk perataan
-  for (let j = 10; j > i; j--) {
-    bintang += " ";
-  }
-  // Tambahkan bintang
-  for (let k = 1; k <= 2 * i - 1; k++) {
-    bintang += "*";
-  }
-  bintang += "\n";
-}
-console.log(bintang);
+alert("terima kasih sudah bermain");
+console.log("game selesai");
