b5bc6139ca6b791283accce5ffea5c8f65785650
imbasicprogrammer <mhabibmhabib05@gmail.com>
2025-01-20 16:50:16 +0700

    perulangan dan pengkondisian bersarang, membuat segitiga

--- script.js
+++ script.js
@@ -1,32 +1,66 @@
-//belajar switch
+//Belajar perulangan dan pengkondisian bersarang
 
-menu = prompt("pilih menu : \n cth: daging, ayam, ikan, burger, pizza, mie instan");
-
-switch (menu) {
-  case "daging":
-    console.log("daging sehat!!");
-    alert("daging sehat");
-    break;
-  case "ayam":
-    console.log("ayam sehat!!");
-    alert("ayam sehat");
-    break;
-  case "ikan":
-    console.log("ikan sehat!!");
-    alert("ayam sehat");
-    break;
-  case "burger":
-    console.log("burger tidak sehat");
-    alert("burger tidak sehat");
-    break;
-  case "pizza":
-    console.log("pizza tidak sehat!!");
-    alert("pizza tidak sehat");
-    break;
-  case "mie instan":
-    console.log("mie instan tidak sehat!!");
-    alert("mie instan tidak sehat");
-    break;
-  default:
-    alert("yang anda masukan salah!!");
+//1. perulangan menampilkan bintang 10 baris dengan masing masing 5 bintang
+/*
+let string = "";
+for (let i = 0; i < 10; i++) {
+  for (let j = 0; j < 5; j++) {
+    string += "*";
+  }
+  string += "\n";
 }
+console.log(string);
+*/
+
+//2. Perulangan menampilkan bintang segitiga
+/*
+let star = "";
+for (let i = 0; i < 10; i++) {
+  for (let j = 0; j <= i; j++) {
+    star += "*";
+  }
+  star += "\n";
+}
+console.log(star);
+*/
+
+//3. perulangan bintang segitiga terbalik
+/*
+let bintang = "";
+for (let i = 10; i > 0; i--) {
+  for (let j = 0; j < i; j++) {
+    bintang += "*";
+  }
+  bintang += "\n";
+}
+console.log(bintang); */
+
+//4. segitiga terbalik berlawanan
+/*
+let bintang = "";
+for (let i = 0; i < 10; i++) {
+  for (let j = 0; j < i; j++) {
+    bintang += " ";
+  }
+  for (let k = 10; k > i; k--) {
+    bintang += "*";
+  }
+  bintang += "\n";
+}
+console.log(bintang); */
+
+//5 Membuat segitiga sama kaki
+
+let bintang = "";
+for (let i = 1; i <= 10; i++) {
+  // Tambahkan spasi untuk perataan
+  for (let j = 10; j > i; j--) {
+    bintang += " ";
+  }
+  // Tambahkan bintang
+  for (let k = 1; k <= 2 * i - 1; k++) {
+    bintang += "*";
+  }
+  bintang += "\n";
+}
+console.log(bintang);
